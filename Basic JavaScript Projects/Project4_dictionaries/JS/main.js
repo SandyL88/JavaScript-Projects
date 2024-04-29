7a558bbcd72df91e251450cd668b467a5405f2be
SandyL88 <SANDY66888866@OUTLOOK.COM>
2024-04-29 01:30:30 -0400

    Project4 dictionary submit
    Write a dictionary with a function that outputs a key-value pair into an HTML element
    
    Write a delete statement that removes the key before its value is displayed
    
    Add comments throughout your JavaScript explaining your code

---
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,33 @@
+function my_dictionary() {
+    var books = [
+        {
+            name: "My Mixed Emotions",
+            author: "Elinor  Greenwood",
+            publishers: ["DK", "Penguin Random House"],
+            published_year: "2018",
+        },
+        {
+            name: "the Monster at the end of this Book",
+            author: "Jon Stone",
+            publishers: "Golden Books",
+            published_year: "2015",
+        },
+        {
+            name: "the Amazing Pop-up Grammar Book",
+            author: "Jennie Maizels & Kate Petty",
+            publishers: "GDutton Children's Books",
+            published_year: "1996",
+        },
+    ];
+    var booksInfo = "";
+    for (var i = 0; i < books.length; i++) {
+        var bookInfo = "Child book: " + books[i].name + "<br>" +
+                        "Author: " + books[i].author + "<br>" +
+                        "Publishers: " + books[i].publishers + "<br>" +
+                        "Published Year: " + books[i].published_year + "<br><br>";
+        booksInfo += bookInfo;
+    }
+
+    delete books[0].publishers;
+    document.getElementById("Dictionary").innerHTML = "Child book:" + booksInfo;
+}
